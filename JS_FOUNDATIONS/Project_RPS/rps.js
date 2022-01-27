c760063ebd8727bbdd6acb2b1347e778b25075a3
Agustinsn <agustinasn95@gmail.com>
2022-01-26 19:14:46 -0500

    rps-final

--- JS_FOUNDATIONS/Project_RPS/rps.js
+++ JS_FOUNDATIONS/Project_RPS/rps.js
@@ -1,3 +1,5 @@
+let player= window.prompt("Rock, paper or scissors?")
+
 let computerPlay =()=>{
     let random=Math.floor(Math.random()*3) 
     if(random===0){
@@ -16,21 +18,27 @@ let playRound=(playerSelection,computerSelection)=>{
     let CS=computerSelection.toLowerCase()
     if (PS !== CS ){
         if(PS ==="rock" && CS==="paper"){
+            computerScore++
             return "You lose! Paper beats rock!"
         }
         if(PS==="rock" && CS==="scissors"){
+            playerScore++
             return "You win! Rock beats scissors!"
         }
         if(PS ==="paper" && CS==="rock"){
+            playerScore++
             return "You win! Paper beats rock!"
         }
         if(PS ==="paper" && CS==="scissors"){
+            computerScore++
             return "You lose! Scissors beats paper!"
         }
         if(PS ==="scissors" && CS==="rock"){
+            computerScore++
             return "You lose! Paper beats rock!"
         }
         if(PS ==="scissors" && CS==="paper"){
+            playerScore++
             return "You win! Scissors beats paper!"
         }
 
@@ -40,7 +48,27 @@ let playRound=(playerSelection,computerSelection)=>{
     }
 }
 
-player= "Rock"
-computer= computerPlay()
+let computerScore=0
+let playerScore=0
+let game=()=>{
 
-console.log(playRound(player,computer))
+    let count=1
+    for (let i=0; i < 5; i++) {
+        let computer= computerPlay()
+        console.log(playRound(player,computer),playerScore,computerScore)
+        count++
+    }
+    if (playerScore < computerScore) {
+        console.log("Game ends. You lose, good luck next time!")
+    }
+    else if(playerScore===computerScore){
+        console.log("Game ends.Tie game! Try again.")
+    } 
+    else{
+        console.log("Games ends. You win! Congratz!!!!")
+    }
+}
+
+
+
+let totalGame=game()
